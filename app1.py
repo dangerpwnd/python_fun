7006de31ba4de189fc5f61b712a34442f851f3ea
Eric Dangerstone <reric2012@gmail.com>
2019-12-02 16:58:32 -0600

    Adding close matches to dict

--- app1.py
+++ app1.py
@@ -1,10 +1,19 @@
 import json
+from difflib import get_close_matches
+
 data = json.load(open("data.json"))
 
+
 def translate(w):
-    wl = w.lower()
-    if wl in data:
-        return data[wl]
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
+        suggest = input("Did you mean %s instead? Y or N " % get_close_matches(w, data.keys(), cutoff=0.8)[0])
+        if suggest == "Y" | suggest == "y":
+            return data[get_close_matches(w,data.keys(), cutoff=0.8)[0]]
+        else:
+            return "Try again"
     else:
         return "The word doesn't exist. Please double check it."
 
