79718e173f11fecf0872d4a23391df2881c40833
Eric Dangerstone <reric2012@gmail.com>
2020-04-02 09:45:05 -0500

    Finished bookstore project

--- bookstore.py
+++ bookstore.py
@@ -2,32 +2,70 @@ from tkinter import *
 import backend
 
 # Functions
+
+
+def get_selected_row(event):
+    try:
+        global selected_tuple
+        index = list1.curselection()[0]
+        selected_tuple = list1.get(index)
+        e1.delete(0, END)
+        e1.insert(END, selected_tuple[1])
+        e2.delete(0, END)
+        e2.insert(END, selected_tuple[2])
+        e3.delete(0, END)
+        e3.insert(END, selected_tuple[3])
+        e4.delete(0, END)
+        e4.insert(END, selected_tuple[4])
+    except IndexError:
+        pass
+
+
 def view_command():
     list1.delete(0, END)
     for row in backend.view():
         list1.insert(END, row)
 
+
 def search_command():
     list1.delete(0, END)
-    for row in backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
-        list1.insert(END,row)
+    for row in backend.search(
+        title_text.get(), author_text.get(), year_text.get(), isbn_text.get()
+    ):
+        list1.insert(END, row)
+
 
 def add_command():
-    backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    backend.insert(
+        title_text.get(), author_text.get(), year_text.get(), isbn_text.get()
+    )
     list1.delete(0, END)
-    list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
+    list1.insert(
+        END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    )
+
 
 def update_command():
-    backend.update()
+    backend.update(
+        selected_tuple[0],
+        title_text.get(),
+        author_text.get(),
+        year_text.get(),
+        isbn_text.get(),
+    )
+
 
 def delete_command():
-    backend.delete()
+    backend.delete(selected_tuple[0])
+
 
 def close_command():
-
+    window.destroy
 
 
 window = Tk()
+
+window.wm_title("Bookstore")
 
 # Labels
 l1 = Label(window, text="Title")
@@ -36,11 +74,11 @@ l1.grid(row=0, column=0)
 l2 = Label(window, text="Author")
 l2.grid(row=0, column=2)
 
-l1 = Label(window, text="Year")
-l1.grid(row=1, column=0)
+l3 = Label(window, text="Year")
+l3.grid(row=1, column=0)
 
-l2 = Label(window, text="ISBN")
-l2.grid(row=1, column=2)
+l4 = Label(window, text="ISBN")
+l4.grid(row=1, column=2)
 
 # Entries
 title_text = StringVar()
@@ -70,20 +108,22 @@ sb1.grid(row=2, column=2, rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+list1.bind("<<ListboxSelect>>", get_selected_row)
+
 # Buttons
-b1 = Button(window, text="View All", width=12, command=view_command)
+b1 = Button(window, text="View all", width=12, command=view_command)
 b1.grid(row=2, column=3)
 
-b2 = Button(window, text="Search Entry", width=12, command=search_command)
+b2 = Button(window, text="Search entry", width=12, command=search_command)
 b2.grid(row=3, column=3)
 
-b3 = Button(window, text="Add Entry", width=12, command=add_command)
+b3 = Button(window, text="Add entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
-b4 = Button(window, text="Update", width=12, command=update_command)
+b4 = Button(window, text="Update selected", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
-b5 = Button(window, text="Delete", width=12, command=delete_command)
+b5 = Button(window, text="Delete selected", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
 b6 = Button(window, text="Close", width=12, command=close_command)
@@ -91,5 +131,3 @@ b6.grid(row=7, column=3)
 
 
 window.mainloop()
-
-
