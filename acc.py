0ffbdacf5a6c349c6514e4671c56293ce3558131
Eric Dangerstone <reric2012@gmail.com>
2020-04-02 13:35:12 -0500

    Finished acct project, added subclass

--- acc.py
+++ acc.py
@@ -15,11 +15,36 @@ class Account:
         with open(self.filepath, 'w') as file:
             file.write(str(self.balance))
 
-account=Account("bal.txt")
-print(account.balance)
-account.withdraw(100)
-account.commit()
-print(account.balance)
-account.deposit(250)
-account.commit()
-print(account.balance)
+# Declare base class
+class Checking(Account):
+
+    def __init__(self, filepath, fee):
+        # Inheriting the class
+        Account.__init__(self, filepath)
+        # Subclass variable
+        self.fee=fee
+
+    def transfer(self, amount):
+        self.balance=self.balance - amount - self.fee
+
+# checking=Checking("bal.txt", 1)
+# checking.transfer(200)
+# print(checking.balance)
+
+# checking=Checking("bal.txt")
+# checking.transfer(200)
+# print(checking.balance)
+
+# checking=Checking("bal.txt")
+# checking.deposit(150)
+# checking.commit()
+# print(checking.balance)
+
+# account=Account("bal.txt")
+# print(account.balance)
+# account.withdraw(100)
+# account.commit()
+# print(account.balance)
+# account.deposit(250)
+# account.commit()
+# print(account.balance)
