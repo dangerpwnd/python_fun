79358a92f2ff67b427df0aceeef01b48499e446f
Eric Dangerstone <reric2012@gmail.com>
2020-04-02 13:21:37 -0500

    Accounting py script for OOP

---
+++ acc.py
@@ -0,0 +1,25 @@
+class Account:
+
+    def __init__(self, filepath):
+        self.filepath=filepath
+        with open(filepath, 'r') as file:
+            self.balance=int(file.read())
+    
+    def withdraw(self, amount):
+        self.balance=self.balance - amount
+        
+    def deposit(self, amount):
+        self.balance=self.balance + amount
+
+    def commit(self, ):
+        with open(self.filepath, 'w') as file:
+            file.write(str(self.balance))
+
+account=Account("bal.txt")
+print(account.balance)
+account.withdraw(100)
+account.commit()
+print(account.balance)
+account.deposit(250)
+account.commit()
+print(account.balance)
