8e3bc274f523e025906bd6a95b7f7ea365e1af10
Eric Dangerstone <reric2012@gmail.com>
2020-04-08 08:39:59 -0500

    Updated for autosizing window based on quote text

--- customquote.py
+++ customquote.py
@@ -5,26 +5,17 @@ from dictor import dictor
 with urllib.request.urlopen("http://quotes.rest/qod.json?category=inspire") as url:
     data = json.loads(url.read().decode())
     quote = dictor(data, 'contents.quotes.0.quote', default="Missing Quote")
-    length = int(dictor(data, 'contents.quotes.0.length', default="Missing Length"))
     author = dictor(data, 'contents.quotes.0.author', default="Missing Author")
 
-# Font, font size
-fnt=ImageFont.truetype(r'C:\Windows\Fonts\segoepr.ttf', 20)
 
-if length > 170:
-    # Mode, (W,H), color
-    img=Image.new('RGB',(2000,100),color=(255,255,255))
-    d=ImageDraw.Draw(img)
-    # (x,y), Text, alignment, font, font color
-    d.multiline_text((35,30),quote,align="center",font=fnt,fill=(0,0,0))
-    d.multiline_text((950,60),author,align="center",font=fnt,fill=(0,0,0))
-elif length > 150:
-    # Mode, (W,H), color
-    img=Image.new('RGB',(1720,100),color=(255,255,255))
-    d=ImageDraw.Draw(img)
-    # (x,y), Text, alignment, font, font color
-    d.multiline_text((25,30),quote,align="center",font=fnt,fill=(0,0,0))
-    d.multiline_text((750,60),author,align="center",font=fnt,fill=(0,0,0))
-
+fnt=ImageFont.truetype(r'C:\Windows\Fonts\segoepr.ttf', 24)
+fntsizequote=list(fnt.getsize(quote))
+fntsizeauthor=list(fnt.getsize(author))
+fntsizew=fntsizequote[0]
+fntsizeh=fntsizequote[1] + fntsizeauthor[1]
+fntsizea=fntsizequote[0] - fntsizeauthor[0] - 25
+img=Image.new('RGB', (fntsizew,fntsizeh), (255,255,255))
+draw=ImageDraw.Draw(img)
+draw.multiline_text((0,0),quote,font=fnt,fill=(0,0,0))
+draw.multiline_text((fntsizea,30),author,font=fnt,fill=(0,0,0))
 img.save(r'C:\Users\Eric.M\Pictures\Quote.png')
-
